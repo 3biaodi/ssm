64082b8504150af7774bfabb5901bafd37192bef
Huaiku <banboll@outlook.com>
2020-09-02 10:57:15 +0800

    测试git

---
+++ src/main/java/com/Pets/controller/indexController.java
@@ -0,0 +1,10 @@
+package com.Pets.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class indexController {
+    public String index(){
+        return "index";
+    }
+}
